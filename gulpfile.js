f644bfb27361bb7fd5f89801a52b2baa6afef647
Erik van Nieuwburg <erik@rumblingskies.com>
2016-02-18 13:20:05 +0100

    added gulp to optimise

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('default', function(){
+	return gulp.src(['./css/normalize.css', './css/maingreen.css'])
+		.pipe(concat('bundle.css'))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+		.pipe(cssnano())
+		.pipe(gulp.dest('./builds/'));
+});
+
+// gulp.src('src/css/**/*.css')
+//     .pipe(cssnano())
+//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+//     .pipe(concat('style.min.css'))
+//     .pipe(gulp.dest('dist/css'))
