595c2703ac0fa574366a1fecd23a5ef21266d65e
Erik van Nieuwburg <erik@rumblingskies.com>
2016-02-18 14:22:52 +0100

    little tweaking for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ var cssnano = require('gulp-cssnano');
 gulp.task('default', function(){
 	return gulp.src(['./css/normalize.css', './css/maingreen.css'])
 		.pipe(concat('bundle.css'))
-		.pipe(uncss({
-			html: ['index.html']
-		}))
+		// .pipe(uncss({
+		// 	html: ['index.html']
+		// }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
 		.pipe(cssnano())
 		.pipe(gulp.dest('./builds/'));
